300535b406ab9bccdc1503276650af725a753587
jhaunn <89385585+jhaunn@users.noreply.github.com>
2023-07-20 09:57:42 +0800

    Removed Watering Feature

--- My project/Assets/Scripts/ActionBuffer.cs
+++ My project/Assets/Scripts/ActionBuffer.cs
@@ -8,18 +8,14 @@ public class ActionBuffer : MonoBehaviour
     public static ActionBuffer instance;
 
     [SerializeField] private float plantBuffer;
-    [SerializeField] private float waterBuffer;
     [SerializeField] private float harvestBuffer;
     private float currentPlantBuffer = 0f;
-    private float currentWaterBuffer = 0f;
     private float currentHarvestBuffer = 0f;
 
     [SerializeField] private Slider plantSlider;
-    [SerializeField] private Slider waterSlider;
     [SerializeField] private Slider harvestSlider;
 
     [HideInInspector] public bool canPlant = false;
-    [HideInInspector] public bool canWater = false;
     [HideInInspector] public bool canHarvest = false;
 
     private void Awake()
@@ -37,18 +33,15 @@ public class ActionBuffer : MonoBehaviour
     private void Start()
     {
         plantSlider.maxValue = plantBuffer;
-        waterSlider.maxValue = waterBuffer;
         harvestSlider.maxValue = harvestBuffer;
     }
 
     private void Update()
     {
         plantSlider.value = currentPlantBuffer;
-        waterSlider.value = currentWaterBuffer;
         harvestSlider.value = currentHarvestBuffer;
 
         currentPlantBuffer += Time.deltaTime;
-        currentWaterBuffer += Time.deltaTime;
         currentHarvestBuffer += Time.deltaTime;
 
         if (currentPlantBuffer >= plantSlider.maxValue)
@@ -58,15 +51,6 @@ public class ActionBuffer : MonoBehaviour
         else
         {
             canPlant = false;
-        }
-
-        if (currentWaterBuffer >= waterSlider.maxValue)
-        {
-            canWater = true;
-        }
-        else
-        {
-            canWater = false;
         }
 
         if (currentHarvestBuffer >= harvestSlider.maxValue)
@@ -82,11 +66,6 @@ public class ActionBuffer : MonoBehaviour
     public void UsePlantAction()
     {
         currentPlantBuffer = 0f;
-    }
-
-    public void UseWaterAction()
-    {
-        currentWaterBuffer = 0f;
     }
 
     public void UseHarvestAction()
