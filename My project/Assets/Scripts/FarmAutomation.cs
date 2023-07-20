c05b2610c206e254dc223917f8888effae69a6a3
jhaunn <89385585+jhaunn@users.noreply.github.com>
2023-07-20 12:01:54 +0800

    Updated Farm Automation

--- My project/Assets/Scripts/FarmAutomation.cs
+++ My project/Assets/Scripts/FarmAutomation.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FarmAutomation : MonoBehaviour
 {
     private FarmPlot[] farmPlots;
 
+    [SerializeField] private GameObject farmNPC;
+    private TextMeshPro npcText;
+    [SerializeField] private float npcInteractRadius;
+    [SerializeField] private LayerMask playerLayerMask;
+
     private void Awake()
     {
         farmPlots = transform.GetComponentsInChildren<FarmPlot>();
+        npcText = farmNPC.GetComponentInChildren<TextMeshPro>();
     }
 
     private void Update()
@@ -28,5 +35,25 @@ public class FarmAutomation : MonoBehaviour
                 }
             }
         }
+
+        if (Physics2D.OverlapCircle(farmNPC.transform.position, npcInteractRadius, playerLayerMask))
+        {
+            npcText.gameObject.SetActive(true);
+            npcText.text = "Interact";
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Interacted with NPC");
+            }
+        }
+        else
+        {
+            npcText.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(farmNPC.transform.position, npcInteractRadius);
     }
 }
