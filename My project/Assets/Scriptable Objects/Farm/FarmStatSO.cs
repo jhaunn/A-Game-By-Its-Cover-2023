b4763d3c69ecd4a80d9839613bafe551e0d00f37
jhaunn <89385585+jhaunn@users.noreply.github.com>
2023-07-31 09:47:29 +0800

    Update FarmStatSO.cs

--- My project/Assets/Scriptable Objects/Farm/FarmStatSO.cs
+++ My project/Assets/Scriptable Objects/Farm/FarmStatSO.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Farm Stat", menuName = "Farm Stat")]
 public class FarmStatSO : ScriptableObject
 {
+    [Header("Unlock Stats")]
     public int farmPrice;
-
     public PlantsSO plant;
 
+    [Header("Base Stats")]
     public float minGrowthSpeed, maxGrowthSpeed;
     public int minYield, maxYield;
+
+    [Header("Upgrade Stats")]
+    public int[] upgradePrice;
+    public float[] upgradeMinGrowthSpeed, upgradeMaxGrowthSpeed;
+    public int[] upgradeMinYield, upgradeMaxYield;
+
 }
