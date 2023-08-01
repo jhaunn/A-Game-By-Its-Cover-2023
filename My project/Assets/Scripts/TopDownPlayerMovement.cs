0fab4dba067224e2aefd3867b45b7ef8dfff80a7
jhaunn <89385585+jhaunn@users.noreply.github.com>
2023-08-01 09:21:47 +0800

    Updated Player Idle Animation

--- My project/Assets/Scripts/TopDownPlayerMovement.cs
+++ My project/Assets/Scripts/TopDownPlayerMovement.cs
@@ -26,6 +26,18 @@ public class TopDownPlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (movement.x > 0f || movement.x < 0f)
+        {
+            animator.SetFloat("LastHorizontal", movement.x);
+            animator.SetFloat("LastVertical", 0f);
+        }
+
+        if (movement.y > 0f || movement.y < 0f)
+        {
+            animator.SetFloat("LastVertical", movement.y);
+            animator.SetFloat("LastHorizontal", 0f);
+        }
     }
 
     private void FixedUpdate()
