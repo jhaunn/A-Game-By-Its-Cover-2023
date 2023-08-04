0fa5fdde8f9303d0e1924834edbb5e426de083d9
jhaunn <89385585+jhaunn@users.noreply.github.com>
2023-08-04 11:52:46 +0800

    Added NPC Spawner

---
+++ My project/Assets/Scripts/NpcSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcSpawner : MonoBehaviour
+{
+    [SerializeField] private float xBound;
+    [SerializeField] private float yBound;
+    [SerializeField] private float spawnTimer;
+    private float currentSpawnTimer;
+
+    [SerializeField] private GameObject npc;
+
+    private void Start()
+    {
+        currentSpawnTimer = spawnTimer;
+    }
+
+    private void Update()
+    {
+        currentSpawnTimer -= Time.deltaTime;
+
+        if (currentSpawnTimer <= 0f)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(xBound, -xBound), Random.Range(yBound, -yBound));
+
+            Instantiate(npc, spawnPos + transform.position, npc.transform.rotation);
+
+            currentSpawnTimer = spawnTimer;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(xBound * 2, yBound * 2));
+    }
+}
